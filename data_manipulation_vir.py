4240917b8e880cc2bb1579762a3dc1b42a2a46ce
Cianca101 <emmanuel.carrillo.c@gmail.com>
2021-02-20 16:45:29 -0600

    Modified data_man_vir

--- data_manipulation_vir.py
+++ data_manipulation_vir.py
@@ -1,12 +1,17 @@
 ### Ejercicio 1
 n = input("Número a multiplicar (1-10): ")
 
-if n >= 1 and n <= 10:
+# if n >= 1 and n <= 10:
+#     for i in range(1, 11):
+#         resultado = n * i
+#         print(f'{n} x {i} = {resultado}')  
+# else:
+#     print("Número fuera de rango")
 
+if n >= 1 and n <= 10:
     for i in range(1, 11):
-        resultado = n * i
-        print(f'{n} x {i} = {resultado}') 
-        
+      resultado = n * i
+      print(f'{n} x {i} = {resultado}') 
 else:
     print("Número fuera de rango")
 
